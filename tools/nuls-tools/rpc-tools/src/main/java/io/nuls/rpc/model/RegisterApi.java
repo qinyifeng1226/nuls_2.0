63788eb98bd6dea849a7516c6f32fc0a21d2ecd9
lijun.zhou <zhouj.172@gmail.com>
2019-03-15 15:07:52 +0800

    修改RegisterApi使用大驼峰序列化

--- tools/nuls-tools/rpc-tools/src/main/java/io/nuls/rpc/model/RegisterApi.java
+++ tools/nuls-tools/rpc-tools/src/main/java/io/nuls/rpc/model/RegisterApi.java
@@ -24,6 +24,8 @@
  */
 package io.nuls.rpc.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 import java.util.List;
@@ -36,28 +38,104 @@ import java.util.Map;
  * @author tangyi
  * @date 2018/11/19
  */
-@Data
 @ToString
 public class RegisterApi {
 
 
+    @JsonProperty("ApiMethods")
     private List<CmdDetail> apiMethods;
 
     /**
      * Key: Role
      * Value: Version
      */
+    @JsonProperty("Dependencies")
     private Map<String, String> dependencies;
 
+    @JsonProperty("ConnectionInformation")
     private Map<String, String> connectionInformation;
 
+    @JsonProperty("ModuleDomain")
     private String moduleDomain;
 
+    @JsonProperty("ModuleRoles")
     private Map<String, String[]> moduleRoles;
 
+    @JsonProperty("ModuleVersion")
     private String moduleVersion;
 
+    @JsonProperty("ModuleAbbreviation")
     private String moduleAbbreviation;
 
+    @JsonProperty("ModuleName")
     private String moduleName;
+
+
+    @JsonIgnore
+    public List<CmdDetail> getApiMethods() {
+        return apiMethods;
+    }
+
+    @JsonIgnore
+    public void setApiMethods(List<CmdDetail> apiMethods) {
+        this.apiMethods = apiMethods;
+    }
+
+    @JsonIgnore
+    public Map<String, String> getDependencies() {
+        return dependencies;
+    }
+
+    @JsonIgnore
+    public void setDependencies(Map<String, String> dependencies) {
+        this.dependencies = dependencies;
+    }
+    @JsonIgnore
+    public Map<String, String> getConnectionInformation() {
+        return connectionInformation;
+    }
+    @JsonIgnore
+    public void setConnectionInformation(Map<String, String> connectionInformation) {
+        this.connectionInformation = connectionInformation;
+    }
+    @JsonIgnore
+    public String getModuleDomain() {
+        return moduleDomain;
+    }
+    @JsonIgnore
+    public void setModuleDomain(String moduleDomain) {
+        this.moduleDomain = moduleDomain;
+    }
+    @JsonIgnore
+    public Map<String, String[]> getModuleRoles() {
+        return moduleRoles;
+    }
+    @JsonIgnore
+    public void setModuleRoles(Map<String, String[]> moduleRoles) {
+        this.moduleRoles = moduleRoles;
+    }
+    @JsonIgnore
+    public String getModuleVersion() {
+        return moduleVersion;
+    }
+    @JsonIgnore
+    public void setModuleVersion(String moduleVersion) {
+        this.moduleVersion = moduleVersion;
+    }
+    @JsonIgnore
+    public String getModuleAbbreviation() {
+        return moduleAbbreviation;
+    }
+    @JsonIgnore
+    public void setModuleAbbreviation(String moduleAbbreviation) {
+        this.moduleAbbreviation = moduleAbbreviation;
+    }
+    @JsonIgnore
+    public String getModuleName() {
+        return moduleName;
+    }
+    @JsonIgnore
+    public void setModuleName(String moduleName) {
+        this.moduleName = moduleName;
+    }
 }
